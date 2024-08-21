99c6af1b3ced26a77ba01e1a43206acc4f41cef1
Tim Bryant <timothybryant3@gmail.com>
2024-08-21 11:01:42 -0400

    feat: Add function to enable podman-auto-update timer and service

--- scripts/rhel-ol/utilities/podman_auto_update_disable.sh
+++ scripts/rhel-ol/utilities/podman_auto_update_disable.sh
@@ -21,7 +21,31 @@ disable_timer_and_service() {
     msg_ok "podman-auto-update.timer and service have been disabled."
 }
 
-# Main script execution
-disable_timer_and_service
+enable_timer_and_service() {
+    msg_info "Enabling podman-auto-update.timer..."
+    sudo systemctl enable podman-auto-update.timer
+    if [ $? -ne 0 ]; then
+        msg_error "Failed to enable podman-auto-update.timer"
+        exit 1
+    fi
 
-msg_ok "Script to disable auto-update completed successfully."
+    msg_info "Enabling podman-auto-update.service..."
+    sudo systemctl enable podman-auto-update.service
+    if [ $? -ne 0 ]; then
+        msg_error "Failed to enable podman-auto-update.service"
+        exit 1
+    fi
+
+    msg_ok "podman-auto-update.timer and service have been enabled."
+}
+
+check_status() {
+    status=$(systemctl status podman-auto-update.timer | grep "Active:" | awk '{print $2}')
+    if [ "$status" = "active" ]; then
+        msg_info "podman-auto-update.timer is active"
+        disable_timer_and_service
+    else
+        msg_info "podman-auto-update timer is not active"
+        enable_timer_and_service
+    fi
+}
