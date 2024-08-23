392674e230117f3b14c55d3474aaab64cd779f63
Tim Bryant <timothybryant3@gmail.com>
2024-08-23 10:04:02 -0400

    feat: Add additional container registries to podman configuration

--- scripts/debian-ubuntu/installers/podman.sh
+++ scripts/debian-ubuntu/installers/podman.sh
@@ -95,7 +95,7 @@ install_podman() {
         return 1
     fi
 
-    if ! echo "unqualified-search-registries = [\"docker.io\",\"quay.io\"]" >>"$config_dir/registries.conf"; then
+    if ! echo "unqualified-search-registries = [\"docker.io\",\"quay.io\",\"container-registry.oracle.com\",\"ghcr.io\",\"lscr.io\"]" >>"$config_dir/registries.conf"; then
         echo_with_color "31" "Failed to add docker.io to registry configuration."
         return 1
     fi
