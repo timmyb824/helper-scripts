66d0d524ae396057d540f51c39bfd896ab1c062e
Tim Bryant <timothy.bryant@simplisafe.com>
2024-11-19 10:47:39 -0500

    fix: Use the specified Go binary path variable instead of relying on system path

--- scripts/rhel-ol/installers/age_sops.sh
+++ scripts/rhel-ol/installers/age_sops.sh
@@ -3,6 +3,8 @@
 # Source necessary utilities
 source "$(dirname "$BASH_SOURCE")/../../init/init.sh"
 
+GO_BIN="/usr/local/go/bin/go"
+
 # Function to install sops on Linux
 install_sops_oracle() {
     if command_exists sops; then
@@ -11,7 +13,7 @@ install_sops_oracle() {
     fi
 
     msg_info "Installing sops with go..."
-    if go install github.com/getsops/sops/v3@latest; then
+    if $GO_BIN install github.com/getsops/sops/v3@latest; then
         # Verify the installation
         if command_exists sops; then
             msg_ok "sops installed successfully on Linux."
@@ -33,7 +35,7 @@ install_age_oracle() {
     fi
 
     msg_info "Installing age with go..."
-    if go install filippo.io/age/cmd/...@latest; then
+    if $GO_BIN install filippo.io/age/cmd/...@latest; then
         # Verify the installation
         if command_exists age; then
             msg_ok "age installed successfully on Linux."
@@ -47,17 +49,12 @@ install_age_oracle() {
     fi
 }
 
-# Check if script is run as root
-if [[ $EUID -ne 0 ]]; then
-    handle_error "This script must be run as root"
-fi
-
 # Check if system is aarch64
 if [[ $(uname -m) != "aarch64" ]]; then
     handle_error "This script is intended for aarch64 architecture only"
 fi
 
-if ! command_exists go; then
+if ! command_exists "$GO_BIN"; then
     msg_error "Go is not installed. Please install Go and try again."
     return 1
 fi
